04443030a037dfc62611ab48f037eee5bc147992
Michael-Selbyecho <michaelselby200@gmail.com>
2025-12-02 21:58:40 +0000

    updated teacher service yml

--- services/school-springboot/teacher-service/src/main/java/com/altera/teacherservice/security/SecurityConfig.java
+++ services/school-springboot/teacher-service/src/main/java/com/altera/teacherservice/security/SecurityConfig.java
@@ -147,7 +147,7 @@ public class SecurityConfig {
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
         configuration.setAllowedOrigins(List.of(
-            "http://localhost:3000",
+            "http://localhost:5173",
             "http://localhost:8085",
             "https://altera.school"
         ));
